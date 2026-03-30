104e0d8454210c1c380f746226678ee21b8038b9
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2026-03-31 00:34:07 +0530

    added_bank_webhook_and_modified_schema

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+  // add zod validations
+  //use webhook secret to choose that the req actually came from hdfc bank.
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInformation.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.json({
+      message: "captured",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+  // update balance in db
+});
